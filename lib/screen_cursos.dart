4173b3a5de376792b35c2fd355d048e50aa3c44e
Victor-2691 <victor.salgado.martinez@cuc.cr>
2022-12-07 21:47:44 -0600

    ok

---
+++ lib/screen_cursos.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class screen_curso extends StatefulWidget {
+  final String tipoid;
+  final String id;
+  final String codigocarre;
+  const screen_curso(this.tipoid, this.id, this.codigocarre, {super.key});
+
+  @override
+  State<screen_curso> createState() => _screen_cursoState();
+}
+
+class _screen_cursoState extends State<screen_curso> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
